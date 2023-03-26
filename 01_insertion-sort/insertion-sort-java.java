0e40f06a20610c8d652ec6c6493482d0087b2ffd
danieaneta <83565907+danieaneta@users.noreply.github.com>
2023-03-25 22:27:30 -0500

    Update insertion-sort-java.java

--- 01_insertion-sort/insertion-sort-java.java
+++ 01_insertion-sort/insertion-sort-java.java
@@ -1,3 +1,7 @@
+int[] arr = {5, 2, 4, 6, 1, 3};
+insertionSort(arr);
+System.out.println(Arrays.toString(arr)); // prints [1, 2, 3, 4, 5, 6]
+
 public static void insertionSort(int[] arr) {
     int n = arr.length;
     for (int i = 1; i < n; i++) {
@@ -10,4 +14,6 @@ public static void insertionSort(int[] arr) {
         arr[j + 1] = key;
     }
 }
+
+
 

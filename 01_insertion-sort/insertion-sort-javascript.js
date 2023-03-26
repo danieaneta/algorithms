f38a271f99048e8a602f42cd2c59f8593f4e27b7
danieaneta <83565907+danieaneta@users.noreply.github.com>
2023-03-25 22:42:51 -0500

    Update insertion-sort-javascript.js

--- 01_insertion-sort/insertion-sort-javascript.js
+++ 01_insertion-sort/insertion-sort-javascript.js
@@ -1,6 +1,4 @@
 let arr = [5, 2, 4, 6, 1, 3];
-insertionSort(arr);
-console.log(arr); // prints [1, 2, 3, 4, 5, 6]
 
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
@@ -13,3 +11,6 @@ function insertionSort(arr) {
     arr[j + 1] = key;
   }
 }
+
+insertionSort(arr);
+console.log(arr); // prints [1, 2, 3, 4, 5, 6]
